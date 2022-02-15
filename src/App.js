d85fee57ff28baf86e60cfd4900d3142f30b4ba6
Laysa Souza <karollinylaysa@gmail.com>
2022-02-15 11:20:10 +0000

    Added react router DOM and wrapped the Homepage component inside Switch and Route components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Route, Switch} from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import './App.css';
 
 function App() {
     return (
         <div>
-            <Homepage/>
+            <Switch>
+                <Route exact path={'/'} component={Homepage}/>
+            </Switch>
         </div>
     );
 }
--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -8,5 +8,5 @@ export default function Homepage() {
         <div className='homepage'>
           <Directory />
         </div>
-    )
+    );
 }
