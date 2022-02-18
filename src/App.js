87f90ccc3d5e12bf4e0d35c54e32bda38dbe5419
Laysa Souza <karollinylaysa@gmail.com>
2022-02-18 14:25:00 +0000

    Added the <CollectionPreview>, <ShopPage> components

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import {Route, Switch} from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
+import ShopPage from "./shop/ShopPage";
+
 import './App.css';
 
 
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-    );
 
 function App() {
     return (
         <div>
             <Switch>
                 <Route exact path={'/'} component={Homepage}/>
-                <Route path={'/shop/hats'} component={HatsPage}/>
+                <Route path={'/shop'} component={ShopPage}/>
             </Switch>
         </div>
     );
