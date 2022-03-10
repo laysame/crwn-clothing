f61f3998d667b32f5f13063be7a5c20499a23ae9
Laysa Souza <karollinylaysa@gmail.com>
2022-03-10 11:22:28 +0000

    Added <Header /> component and links to homepage, shopage and contact

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React from 'react';
 import {Route, Switch} from "react-router-dom";
 import Homepage from "./pages/homepage/Homepage";
 import ShopPage from "./shop/ShopPage";
+import Header from "./components/header-component/Header";
 
 import './App.css';
-
-
 
 function App() {
     return (
         <div>
+            <Header />
             <Switch>
                 <Route exact path={'/'} component={Homepage}/>
                 <Route path={'/shop'} component={ShopPage}/>
@@ -19,3 +19,6 @@ function App() {
 }
 
 export default App;
+
+
+//Header outside the Switch and Route components. It must be independent
