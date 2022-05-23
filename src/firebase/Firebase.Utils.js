87fed3f2ab2e8d00a91266905aa8a0a684c58c7e
Laysa Souza <karollinylaysa@gmail.com>
2022-05-23 23:40:36 +0100

    Read and Write user data on firestore

--- src/firebase/Firebase.Utils.js
+++ src/firebase/Firebase.Utils.js
@@ -2,9 +2,11 @@ import firebase from "firebase/compat";
 import 'firebase/compat/firestore';
 import 'firebase/compat/auth';
 
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth, signInWithPopup } from "firebase/auth";
+import {initializeApp} from "firebase/app";
+import {getAuth, signInWithPopup} from "firebase/auth";
+import {getFirestore} from "firebase/firestore";
+import {doc, setDoc, getDoc} from "firebase/firestore";
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyA8r6Fd8hgiTxcOyMS0hh47CWJeRjWkg5Y",
@@ -16,14 +18,37 @@ const firebaseConfig = {
     measurementId: "G-9CRG340MT9"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+const db = getFirestore(app);
 export const auth = getAuth(app);
+
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return; //exits the function
+
+    const userRef = doc(db, 'users', userAuth.uid);
+    const snapShot = await getDoc(userRef);
+
+    if (!snapShot.exists()) {
+        try {
+            await setDoc(userRef, {
+                email: userAuth.email,
+                name: userAuth.displayName,
+                createdAt: new Date()
+            }, {merge: true});
+
+        } catch (error) {
+            console.log('Error creating user', error.message);
+        }
+        return userRef;
+    }
+
+}
 
 const provider = new firebase.auth.GoogleAuthProvider();
 provider.setCustomParameters({prompt: 'select_account'});
 export const signInWithGoogle = () => signInWithPopup(auth, provider);
-//
-// export default firebase;
+
+
