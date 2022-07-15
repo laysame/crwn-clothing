6aef8a8eaf938db7b02a25e8887a3e084798222b
Laysa Souza <karollinylaysa@gmail.com>
2022-07-15 17:53:34 +0100

    Converted class component into a function component

--- src/App.js
+++ src/App.js
@@ -8,66 +8,21 @@ import Authentication from './pages/authentication/Authentication';
 
 import './App.css';
 
-class App extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            currentUser: null
-        };
-    }
-
-    unsubscribeFromAuth = null;
-
-    componentDidMount() {
-/*
- this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-
-            // make sure that the current user is got from the object of userAuth - changed the parameter user to userAuth
-            if (userAuth) {
-                const userRef = await createUserProfileDocument(userAuth); //get back the object using this function
-
-                const unsub = onSnapshot(doc(db, "users", userAuth.uid), (snapshot) => {
-                    this.setState({
-                        currentUser: {
-                            id: snapshot.id,
-                            ...snapshot.data()
-                        }
-                    })
-                    console.log("Current data: ", snapshot.data());
-                    console.log("UserRef:", userRef );
-                });
-
-            } else {
-                this.setState({currentUser: null});
-            }
-        })
- */
-    }
-
-    componentWillUnmount() {
-        this.unsubscribeFromAuth();
-    }
-
-
-    render() {
-        return (
-            <div>
-                <Header currentUser={this.state.currentUser}/>
-                <Switch>
-                    <Route exact path={'/'} component={Homepage}/>
-                    <Route path={'/shop'} component={ShopPage}/>
-                    <Route path={'/auth'}
-                           render={(props) =>
-                               <Authentication {...props}/>}
-                    />
-                </Switch>
-            </div>
-        );
-    }
+export default function App() {
+    return (
+        <div>
+            <Header/>
+            <Switch>
+                <Route exact path={'/'} component={Homepage}/>
+                <Route path={'/shop'} component={ShopPage}/>
+                <Route path={'/auth'}
+                       render={(props) =>
+                           <Authentication {...props}/>}
+                />
+            </Switch>
+        </div>
+    )
 }
-
-export default App;
 
 
 //Header outside the Switch and Route components. It must be independent
