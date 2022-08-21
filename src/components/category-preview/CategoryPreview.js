950bf2e154822fe2e6d8d81bd567d069aaf2094e
Laysa Souza <karollinylaysa@gmail.com>
2022-08-21 17:54:04 +0100

    added routeChange() that handles the click on the title of each category pushing the history's path

--- src/components/category-preview/CategoryPreview.js
+++ src/components/category-preview/CategoryPreview.js
@@ -1,12 +1,23 @@
 import React from "react";
 import './CategoryPreview.scss';
 import ProductCard from "../product-card/ProductCard";
+import {useHistory} from "react-router-dom";
+
 
 export default function CategoryPreview({title, products}) {
+
+    const history = useHistory();
+    const routeChange = () => {
+        const path = `/shop/${title}`;
+        history.push(path);
+
+        console.log(path)
+    }
+
     return (
         <div className='category-preview-container'>
             <h2>
-                <span className='title'>{title.toUpperCase()}</span>
+                <span className='title' onClick={routeChange}>{title.toUpperCase()}</span>
             </h2>
             <div className='preview'>
                 {
